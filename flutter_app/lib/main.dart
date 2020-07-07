7207f82b964a605fce84378f1a30115aa39cf68f
SoerenBak <34088652+SoerenBak@users.noreply.github.com>
2020-07-07 13:29:08 +0200

    Stateless test

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,27 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: ProfileCard(),
 ));
 
-class Home extends StatelessWidget {
+class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]
+        title: Text('Profile ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        color: Colors.blueGrey,
-        child: Text('Hello World'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[600],
-        child: Text('click'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/profile-img.png'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Soren',
+              style: TextStyle(
+              color: Colors.amberAccent[200],
+              letterSpacing: 2.0,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Current Level',
+              style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+              fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              '2',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'Soren@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+
